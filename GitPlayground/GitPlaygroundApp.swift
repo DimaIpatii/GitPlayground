007bd09e39d5fd597c8503246e395964fabbd4d0
Dimitrii <32913446+DimaIpatii@users.noreply.github.com>
2024-07-29 08:05:40 +0200

    Initial Commit

---
+++ GitPlayground/GitPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPlaygroundApp.swift
+//  GitPlayground
+//
+//  Created by  Ipatii Dmytro on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GitPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
