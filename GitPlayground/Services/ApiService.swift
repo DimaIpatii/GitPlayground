eb177604625720353a745362c13dd2f383409b2c
DEMETRIUS IPATII <32913446+DimaIpatii@users.noreply.github.com>
2024-08-02 22:02:14 +0200

    [Feature] Account Service (#4)
    * [Features] Added print into fetch paragraph function

---
+++ GitPlayground/Services/ApiService.swift
@@ -0,0 +1,21 @@
+//
+//  ApiService.swift
+//  GitPlayground
+//
+//  Created by  Ipatii Dmytro on 02/08/24.
+//
+
+import Foundation
+
+
+class AppService {
+    init(){
+        
+    }
+}
+
+extension AppService {
+    func fetchParagraphs() -> Void {
+     print("Fetched data")
+    }
+}
