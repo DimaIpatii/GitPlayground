e330abdee4d2eccf91afd35a98d1a1acd523c552
Dimitrii <32913446+DimaIpatii@users.noreply.github.com>
2024-08-02 22:10:34 +0200

    [Feature] Created my account view

---
+++ GitPlayground/Views/MyAccountView.swift
@@ -0,0 +1,22 @@
+//
+//  MyAccountView.swift
+//  GitPlayground
+//
+//  Created by  Ipatii Dmytro on 02/08/24.
+//
+
+import SwiftUI
+
+struct MyAccountView: View {
+    var body: some View {
+        VStack{
+            Text("Hello!")
+        }
+    }
+}
+
+struct MyAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyAccountView()
+    }
+}
