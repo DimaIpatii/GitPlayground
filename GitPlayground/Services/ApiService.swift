ca0a459769bdbd8b822bab4a6098b1c0af2af4c2
Dimitrii <32913446+DimaIpatii@users.noreply.github.com>
2024-08-02 21:16:25 +0200

    [Feature] Configure service class

---
+++ GitPlayground/Services/ApiService.swift
@@ -0,0 +1,19 @@
+//
+//  ApiService.swift
+//  GitPlayground
+//
+//  Created by  Ipatii Dmytro on 02/08/24.
+//
+
+import Foundation
+
+
+class AppService {
+    init(){
+        
+    }
+}
+
+extension AppService {
+    
+}
